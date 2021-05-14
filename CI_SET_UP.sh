d773b2ae9e58647f346e5a07790944b8e674bf69
Krasen Parvanov <krasenparvanov@gmail.com>
2021-05-14 15:33:49 +0200

    #21 Update bash dependency
    Co-Authored-By: Alexander Hultén <hultena@student.chalmers.se>
    Co-Authored-By: Eemil Jeskanen <jeskanen@student.chalmers.se>
    Co-Authored-By: Chrysostomos Tsagkidis <chrtsa@student.chalmers.se>

--- CI_SET_UP.sh
+++ CI_SET_UP.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-apk add --update --no-cache python3 wget unzip py3-pip chromium python3-dev py-numpy-dev && ln -sf python3 /usr/bin/python
+apk add --update --no-cache tesseract-ocr python3 wget unzip py3-pip chromium && ln -sf python3 /usr/bin/python
 python -m ensurepip
-pip3 install --no-cache --upgrade pip setuptools wheel
+pip3 install --no-cache --upgrade pip setuptools wheel && \
+apk add --no-cache --virtual .build-deps gcc g++ zlib-dev make python3-dev py-numpy-dev jpeg-dev && \
+    pip3 install matplotlib && \
+    apk del .build-deps
 wget https://chromedriver.storage.googleapis.com/90.0.4430.24/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
-pip3 install selenium matplotlib
+pip3 install selenium
 
