b124b15fcb46c640e14bf267a34ace0e7bbec7da
Krasen Parvanov <krasenparvanov@gmail.com>
2021-05-16 12:08:29 +0200

    #21 Update runSteering
    Co-Authored-By: Alexander Hultén <hultena@student.chalmers.se>
    Co-Authored-By: Eemil Jeskanen <jeskanen@student.chalmers.se>
    Co-Authored-By: Chrysostomos Tsagkidis <chrtsa@student.chalmers.se>

--- performance_testing/scripts/runSteering.sh
+++ performance_testing/scripts/runSteering.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 xhost +
-docker run --rm --net=host --ipc=host --name=steering -v /tmp:/tmp -v $PWD/csv:/csv steering:latest --cid=253 --name=img --width=640 --height=480 &
+#docker run --rm --net=host --ipc=host --name=steering -v /tmp:/tmp -v /home/krasen/Dokument/DIT638/2021-group-04/csv_files:/csv_files steering:latest --cid=253 --name=img --width=640 --height=480 &
+docker run --rm --net=host --ipc=host --name=steering -v /tmp:/tmp -v $PWD/csv_files:/csv_files steering:latest --cid=253 --name=img --width=640 --height=480 &
 echo "Running Steering" &
 wait
