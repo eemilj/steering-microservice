5390b1d8746fb239ff2c4afa80d98c88021ba461
Krasen Parvanov <krasenparvanov@gmail.com>
2021-05-16 14:46:56 +0200

    #21 Update docker-compose dependency
    Co-Authored-By: Alexander Hultén <hultena@student.chalmers.se>
    Co-Authored-By: Eemil Jeskanen <jeskanen@student.chalmers.se>
    Co-Authored-By: Chrysostomos Tsagkidis <chrtsa@student.chalmers.se>

--- CI_SET_UP.sh
+++ CI_SET_UP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apk add --update py-pip python3-dev libffi-dev openssl-dev gcc libc-dev rust cargo make
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)"
 chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+ls /usr/local/bin/
 docker-compose --version
