96c82f15a769b2c981ea4432ae5684a6e643831d
Krasen Parvanov <krasenparvanov@gmail.com>
2021-05-16 12:02:45 +0200

    #21 Update run script and remove unnecessary files in order to transition to docker-compose build
    Co-Authored-By: Alexander Hultén <hultena@student.chalmers.se>
    Co-Authored-By: Eemil Jeskanen <jeskanen@student.chalmers.se>
    Co-Authored-By: Chrysostomos Tsagkidis <chrtsa@student.chalmers.se>

--- performance_testing/scripts/runOpendlv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker run --rm --init --net=host --ipc=host --name=opendlv-vehicle-view -v /recordings:/opt/vehicle-view/recordings -v /var/run/docker.sock:/var/run/docker.sock -p 8081:8081 chalmersrevere/opendlv-vehicle-view-multi:v0.0.60 &
-echo "Running OpenDLV" &
-wait
-
--- performance_testing/scripts/runScripts.sh
+++ performance_testing/scripts/runScripts.sh
@@ -1,27 +1,26 @@
 #!/bin/sh
-
-service docker start
-
-mkdir testCSV
-sleep 10
-./runH264.sh &
-sleep 10
-ls recordings
+echo STARTING SCRIPT
+ls
+echo STARTING Selenium
 python3 testSelenium.py
 python3 runSelenium.py memory
-
 
 for i in 1 2 3 4 5
 do
   ./runSteering.sh &
   python3 runSelenium.py $i
   echo Done with selenium
-  cp -r csv testCSV
+  ls
+  cp -r csv_files testCSV
   mv ./testCSV/csv/csvOutput.csv ./testCSV/csv/$i.csv
   python3 plotCSV.py $i
   echo Done with plotting
+
   docker stop steering
   
 done
 
+docker stop h264
+docker stop opendlv-vehicle-view
+exit 0
 
