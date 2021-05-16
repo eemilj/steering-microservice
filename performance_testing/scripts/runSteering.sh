5c22cc13cb0743403c901978800c912ef999a4b6
Krasen Parvanov <krasenparvanov@gmail.com>
2021-05-16 15:27:09 +0200

    #21 Change order call for compose build
    Co-Authored-By: Alexander Hultén <hultena@student.chalmers.se>
    Co-Authored-By: Eemil Jeskanen <jeskanen@student.chalmers.se>
    Co-Authored-By: Chrysostomos Tsagkidis <chrtsa@student.chalmers.se>

--- performance_testing/scripts/runSteering.sh
+++ performance_testing/scripts/runSteering.sh
@@ -2,6 +2,6 @@
 
 xhost +
 #docker run --rm --net=host --ipc=host --name=steering -v /tmp:/tmp -v /home/krasen/Dokument/DIT638/2021-group-04/csv_files:/csv_files steering:latest --cid=253 --name=img --width=640 --height=480 &
-docker run --rm --net=host --ipc=host --name=steering -v /tmp:/tmp -v $PWD/csv_files:/csv_files steering:latest --cid=253 --name=img --width=640 --height=480 &
+docker run --rm --net=host --ipc=host --name=steering -v /tmp:/tmp -v $PWD/csv_files:/csv_files group4:latest --cid=253 --name=img --width=640 --height=480 &
 echo "Running Steering" &
 wait
