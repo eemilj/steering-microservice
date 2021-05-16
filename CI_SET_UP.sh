9190b4ed59b1fb1cbe65db7f675244f84dc756aa
Krasen Parvanov <krasenparvanov@gmail.com>
2021-05-16 12:01:57 +0200

    #21 Update entry point for selenium image and change dependency in CI set-up
    Co-Authored-By: Alexander Hultén <hultena@student.chalmers.se>
    Co-Authored-By: Eemil Jeskanen <jeskanen@student.chalmers.se>
    Co-Authored-By: Chrysostomos Tsagkidis <chrtsa@student.chalmers.se>

--- CI_SET_UP.sh
+++ CI_SET_UP.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-apk add --update --no-cache tesseract-ocr python3 wget unzip py3-pip chromium && ln -sf python3 /usr/bin/python
-python -m ensurepip
-pip3 install --no-cache --upgrade pip setuptools wheel && \
-apk add --no-cache --virtual .build-deps gcc g++ zlib-dev make python3-dev py-numpy-dev jpeg-dev && \
-    pip3 install matplotlib && \
-    apk del .build-deps
-wget https://chromedriver.storage.googleapis.com/90.0.4430.24/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-pip3 install selenium
+apk add --update --no-cache py-pip python3-dev libffi-dev openssl-dev gcc libc-dev rust cargo make
 
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
