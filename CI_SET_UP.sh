8e4716d1de14dbf3937a267f1d8f6d3e1e9968fe
Krasen Parvanov <krasenparvanov@gmail.com>
2021-05-16 18:52:12 +0200

    #21 Clean up scripts and remove unused files
    Co-Authored-By: Alexander Hultén <hultena@student.chalmers.se>
    Co-Authored-By: Eemil Jeskanen <jeskanen@student.chalmers.se>
    Co-Authored-By: Chrysostomos Tsagkidis <chrtsa@student.chalmers.se>

--- CI_SET_UP.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-apk add --update py-pip python3-dev libffi-dev openssl-dev gcc libc-dev rust cargo make
-curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)"
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-chmod +x /usr/bin/docker-compose
--- performance_testing/scripts/runScripts.sh
+++ performance_testing/scripts/runScripts.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 echo STARTING SCRIPT
-ls
-echo STARTING Selenium
-python3 testSelenium.py
 python3 runSelenium.py memory
 
 for i in 1 2 3 4 5
@@ -10,14 +7,11 @@ do
   ./runSteering.sh &
   python3 runSelenium.py $i
   echo Done with selenium
-  ls
   cp -r csv_files testCSV
   mv ./testCSV/csv/csvOutput.csv ./testCSV/csv/$i.csv
   python3 plotCSV.py $i
   echo Done with plotting
-
   docker stop steering
-  
 done
 
 docker stop h264
