db0745aed06f53bf91782ce550aea793a2f2c69f
Krasen Parvanov <krasenparvanov@gmail.com>
2021-05-19 21:30:53 +0200

    #21 Update error message for plotting
    Co-Authored-By: Alexander Hultén <hultena@student.chalmers.se>
    Co-Authored-By: Eemil Jeskanen <jeskanen@student.chalmers.se>
    Co-Authored-By: Chrysostomos Tsagkidis <chrtsa@student.chalmers.se>

--- performance_testing/scripts/plotCSV.py
+++ performance_testing/scripts/plotCSV.py
@@ -5,11 +5,12 @@ import pandas as pd
 dataFile = pd.read_csv('./outputCSV/' + sys.argv[1] + '.csv')
 
 plt.plot(dataFile['Timestamp'], dataFile['CalculatedSteeringAngle'])
+
 try:
     oldFile = pd.read_csv('./steering_CSV/' + sys.argv[1] + '.csv')
     plt.plot(dataFile['Timestamp'], oldFile['CalculatedSteeringAngle'], color='blue', label="PreviousSteeringAngle")
 except Exception:
-    print("No such file")
+    print("No such file found")
 
 plt.plot(dataFile['Timestamp'], dataFile['CalculatedSteeringAngle'], color='orange',linestyle='dashed', label="CalculatedSteeringAngle")
 plt.plot(dataFile['Timestamp'], dataFile['ActualGroundSteering'], color='green', label="ActualGroundSteering")
