2733417ee350fa0535d3505c9594c2328610dd85
Krasen Parvanov <krasenparvanov@gmail.com>
2021-05-20 08:26:32 +0200

    #21 Add percentage calculations
    Add percentage calculations for valid frames in python test script
    
    Co-Authored-By: Alexander Hultén <hultena@student.chalmers.se>
    Co-Authored-By: Eemil Jeskanen <jeskanen@student.chalmers.se>
    Co-Authored-By: Chrysostomos Tsagkidis <chrtsa@student.chalmers.se>

--- performance_testing/scripts/plotCSV.py
+++ performance_testing/scripts/plotCSV.py
@@ -2,19 +2,39 @@ import matplotlib.pyplot as plt
 import sys
 import pandas as pd
 
-dataFile = pd.read_csv('./outputCSV/' + sys.argv[1] + '.csv')
+dataFile = pd.read_csv('./oldCSV/' + sys.argv[1] + '.csv')
 oldFile = pd.read_csv('./steering_CSV/' + sys.argv[1] + '.csv')
 
 plt.plot(dataFile['Timestamp'], dataFile['CalculatedSteeringAngle'])
 plt.plot(dataFile['Timestamp'], oldFile['CalculatedSteeringAngle'], color='blue', label="PreviousSteeringAngle")
-plt.plot(dataFile['Timestamp'], dataFile['CalculatedSteeringAngle'], color='orange',linestyle='dashed', label="CalculatedSteeringAngle")
+plt.plot(dataFile['Timestamp'], dataFile['CalculatedSteeringAngle'], color='orange', linestyle='dashed',
+         label="CalculatedSteeringAngle")
 plt.plot(dataFile['Timestamp'], dataFile['ActualGroundSteering'], color='green', label="ActualGroundSteering")
 
 plt.xlabel('Timestamp')
 plt.ylabel('CalculatedSteering/ActualGroundSteering/PreviousSteering')
+currentSteering = dataFile['CalculatedSteeringAngle']
+actualSteering = dataFile['ActualGroundSteering']
+frameCounter = 0
+realFrameCounter = len(actualSteering)
+i = 0
+for steering in actualSteering:
+    upperBound = actualSteering[i] + 0.5 * actualSteering[i]
+    lowerBound = actualSteering[i] - 0.5 * actualSteering[i]
+    if ((actualSteering[i] > 0) and (currentSteering[i] >= lowerBound) and (currentSteering[i] <= upperBound)):
+        frameCounter += 1
+    elif (((actualSteering[i] < 0) and (currentSteering[i] <= lowerBound) and (currentSteering[i] >= upperBound))):
+        frameCounter += 1
+    else:
+        if (currentSteering[i] <= 0.05 and currentSteering[i] >= -0.05):
+            frameCounter += 1
+    realFrameCounter += 1
+    i += 1
+
+percentage = frameCounter / realFrameCounter * 100
 plt.plot([], [], ' ', label="Previous valid frame Percentage:")
-plt.plot([], [], ' ', label="Current valid frame Percentage:")
+plt.plot([], [], ' ', label="Current valid frame Percentage:" + percentage.__str__())
 plt.legend()
-plt.savefig('./graphs/' + sys.argv[1] +'.png')
+plt.savefig('./graphs/' + sys.argv[1] + '.png')
 
 sys.exit()
