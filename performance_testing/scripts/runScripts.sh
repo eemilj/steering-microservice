78e2bcfab94bccb22d12138ffbecbee1ccb5ca3f
Alexander Hultén <gusalexhu@student.gu.se>
2021-05-25 15:39:08 +0200

    #21 Automate reading of filenames
    This attempt to automate the reading of filenames in order to enable the system to run with any number of recordings.
    
    Co-Authored-By: Krasen Parvanov <krasen@student.chalmers.se>
    Co-Authored-By: Eemil Jeskanen <jeskanen@student.chalmers.se>
    Co-Authored-By: Chrysostomos Tsagkidis <chrtsa@student.chalmers.se>

--- performance_testing/scripts/runScripts.sh
+++ performance_testing/scripts/runScripts.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
 echo STARTING SCRIPT
-python3 runSelenium.py memory
+cd /recordings
+ITEMS=$(ls | wc -l)
+echo $ITEMS
+ARRAY=(*.rec)
+cd ..
+python3 runSelenium.py memory "${ARRAY[0]}"
 sleep 5
-
-for i in 1 2 3 4 5
+for ((i=0; i < $ITEMS; i++));
 do
+  echo "${ARRAY[$i]}"
   echo Start Microservice
   ./runSteering.sh &
-  python3 runSelenium.py $i
+  python3 runSelenium.py "${ARRAY[$i]}"
   echo Done with selenium
   ls csv_files
   cp -a csv_files/. outputCSV
-  mv ./outputCSV/csvOutput.csv ./outputCSV/$i.csv
+  mv ./outputCSV/csvOutput.csv ./outputCSV/"${ARRAY[$i]}".csv
 
   mkdir tmpCSV
-  cp ./outputCSV/$i.csv tmpCSV
+  cp ./outputCSV/"${ARRAY[$i]}".csv tmpCSV
 
-  python3 plotCSV.py $i
+  python3 plotCSV.py "${ARRAY[$i]}"
   echo Done with plotting
   docker stop steering
 done
